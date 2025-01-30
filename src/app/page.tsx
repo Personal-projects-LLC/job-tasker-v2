6a031d0f67c8d8de6c957fa9b6b957433d0a0cc2
Samraffi <samvel.vardanyan.developer@gmail.com>
2025-01-31 00:52:58 +0400

    remove test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import React from 'react';
-import { Cards, Container, Footer, Header, Main } from '@/components';
+// import { Cards, Container, Footer, Header, Main } from '@/components';
 
 const Home = () => {
-  return (
-    <Container>
-      <Header />
-      <Main />
-      <Cards />
-      <Footer />
-    </Container>
-  );
+  return 1;
 };
 
 export default Home;
