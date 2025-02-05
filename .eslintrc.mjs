c7b8962d311e583734605dbcc971c58630102041
Samraffi <samvel.vardanyan.developer@gmail.com>
2025-02-05 04:55:27 +0400

    update ESLint configuration and Sonar project settings

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -1,6 +1,13 @@
 export default [
   {
-    extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next'],
+    // Наследуем базовые рекомендации ESLint, TypeScript, Prettier и настройки для Next.js.
+    extends: [
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'next'
+    ],
+    // Определяем переопределения для файлов с тестами.
     overrides: [
       {
         files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
@@ -10,22 +17,37 @@ export default [
         }
       }
     ],
+    // Определяем глобальные переменные среды.
     env: {
-      "browser": true,
-      "node": true,
-      "es2021": true,
-      "jest": true
+      browser: true,
+      node: true,
+      es2021: true,
+      jest: true,
     },
-
+    // Указываем, какие файлы учитывать.
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    // Настройки языка и парсера для TypeScript.
     languageOptions: {
+      parser: '@typescript-eslint/parser',
       parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
         project: ['./tsconfig.json']
       },
       globals: {
+        // Можно добавить декларации глобальных объектов, например:
+        // window: 'readonly',
+        // process: 'readonly',
         ...globals.browser,
         ...globals.node
       }
     },
+    // Правила линтинга.
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'double'],
+      semi: ['error', 'always']
+    }
   }
 ];
